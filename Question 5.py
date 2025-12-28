2a178d29ac794d734d57fb312de5e851c83b63a2
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 19:23:27 +0530

    Create Question 5.py

---
+++ Question 5.py
@@ -0,0 +1,37 @@
+'''
+Most Frequent Numbers form the input
+Write a program to find the most frequent numbers from the given numbers in the input. The most frequent values should be printed in ascending order.
+
+NOTE: This is an I/O type question, you need to write the whole code for taking input and printing the output.
+
+Input Format
+
+The first line contains an integer n
+The next n lines each contain n numbers.
+Output Format
+
+A single list containing the most frequent numbers sorted in ascending order.
+Example
+
+Input
+
+10
+3
+5
+3
+2
+5
+8 
+3
+8 
+5
+0
+Output
+
+[3, 5]
+Explanation
+
+First line contains the number of inputs 10. And the input has 10 more numbers. From the numbers 3 and 5 occurs thrice in the list which is the maximum number of times any number is repeated.
+
+Thus the output is [3, 5] which is printed as a list.
+'''
