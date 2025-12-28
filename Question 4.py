e4185ac3733680605e4dbaf8f9c8432c8fd5e86c
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 19:22:35 +0530

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,13 @@
+'''
+Merge two dictionaries and sum on conflicts
+Write a program to merge two dictionaries. If a key is present in both dictionaries, sum the values.
+
+Assume the values are numbers(int or float).
+
+Example
+
+>>> d1 = {'a': 1, 'b': 2}
+>>> d2 = {'b': 3, 'c': 4}
+>>> merge_dictionaries(d1, d2), 
+{'a': 1, 'b': 5, 'c': 4}
+'''
