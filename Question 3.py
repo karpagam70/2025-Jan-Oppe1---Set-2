64470c56658799460eefbf1c42b200752e261688
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 19:21:00 +0530

    Create Question 3.py

---
+++ Question 3.py
@@ -0,0 +1,14 @@
+'''
+Find missing number in a range of numbers
+Given a list with numbers from 1 to n (inclusive) except one number in the range, write a function to find the missing number.
+
+Assume that only one number from the range will be missing in the given list.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Example
+
+Given the inputs nums=[1, 4, 2, 4, 6, 5, 6] and n is 6.
+
+The number 3 is missing from the range. So, find_missing_number(nums) should return 3 as it is the missing number.
+'''
