5bddc62c69a5e294ee355959c90ecf4bda2b3167
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 19:11:02 +0530

    Create Question 1.py

---
+++ Question 1.py
@@ -0,0 +1,15 @@
+'''
+Check If a number divides two other numbers
+Write a function is_both_divisible_by that takes three integers a, b and c as input and checks if a and b numbers are divisible by c. Assume that c number is non-zero.
+
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Example
+
+>>>is_both_divisible_by(10, 15, 5) # both 10 and 15 are divisible by 5
+True
+>>>is_both_divisible_by(10, 13, 5) # 13 is not divisible by 5
+False
+>>>is_both_divisible_by(19, 15, 5) # 19 is not divisible by 5
+False
+'''
